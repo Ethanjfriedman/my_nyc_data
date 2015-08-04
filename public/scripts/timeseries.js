5ce22d4c6045c4bd9807260478413fe9ccf88f07
Ethan Friedman <ethanjfriedman@gmail.com>
2015-08-04 13:57:33 -0400

    still messing around

--- public/scripts/timeseries.js
+++ public/scripts/timeseries.js
@@ -18,7 +18,6 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-//HOOOWAH
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.value); });
@@ -28,6 +27,54 @@ var svg = d3.select("body").append("svg")
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("Firearms_Discharge_Report.csv", function(error, data) {
+  if (error) throw error;
+  console.log(data);
+  // var maxY = d3.max(data.data, function(d) {
+  //   return parseInt(d[10]);
+  // })
+  //
+  // var seriesNames = [];
+  // var dates = [];
+  // var seriesValues = []
+  // for (var i = 0; i < data.data.length; i++) {
+  //   seriesNames.push(data.data[i][8]);
+  //   }
+  //
+  // for (var i = 9; i < data.meta.view.columns.length; i++) {
+  //   dates.push(data.meta.view.columns[i].name);
+  //   seriesValues.push(parseInt(data.data[0][i]));
+  // }
+  //
+  // x.domain(d3.extent(dates, function(d) { return parseDate(d); })); //should be the years
+  // y.domain([0, maxY]);
+
+  debugger;
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Number of incidents");
+
+
+  svg.append("path")
+      .datum(seriesValues, dates)
+      .attr("class", "line")
+      .attr('stroke', 'green')
+      .attr("d", line);
+
+
+});
 
 // d3.json('firearms_discharge.json', function(error, data) {
 //   if (error) throw error;
@@ -90,38 +137,3 @@ var svg = d3.select("body").append("svg")
 //   y.domain(d3.extent(data, function(d) { return d.value; }));
 // })
 //
-
-d3.csv("adversarial.csv", function(error, data) {
-  if (error) throw error;
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.value = +d.value;
-  });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.value; }));
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Number of incidents");
-
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr('stroke', 'green')
-      .attr("d", line);
-
-
-});
